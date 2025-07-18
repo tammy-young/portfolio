0abe8f88ffa6a5bd9352ee365c272cdbecb663ab
Tammy Young <tammy.q.young@gmail.com>
2025-07-18 02:16:35 -0600

    feat: conditionally show scroll to top button

--- src/components/scrollToTopButton.js
+++ src/components/scrollToTopButton.js
@@ -1,8 +1,20 @@
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { useEffect, useState } from 'react';
 
 const ScrollToTopButton = () => {
+  const [showScrollToTop, setShowScrollToTop] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowScrollToTop(window.pageYOffset > 300);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div>
+    showScrollToTop && (
       <button
         className="fixed bottom-4 right-4 p-3 rounded-full bg-main text-white shadow-lg font-bold hover:bg-main-light dark:hover:shadow-neutral-800 hover:shadow-xl transition-colors duration-300 flex justify-center items-center"
         onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
@@ -10,7 +22,7 @@ const ScrollToTopButton = () => {
       >
         <ArrowUpwardIcon />
       </button>
-    </div>
+    )
   );
 }
 
